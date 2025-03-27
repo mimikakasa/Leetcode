79a50a35a7f74db809db6b89ce6085a7b487be78
mimikakasa <zhoujay231@gmail.com>
2025-03-27 15:57:40 +0800

    Add files via upload

---
+++ solutions/easy/array/binarySearchNextLevel.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int len = nums.size();
+        int left = 0, right = len - 1;
+        int ans = 0;
+        while (left<=right)
+        {
+
+        }
+       
+    }
+};
